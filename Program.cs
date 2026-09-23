2cd175645ba924c35a89a2e60dea040812826730
ninanqui zarate carlos <danipko110533@gmail.com>
2026-09-23 09:01:55 -0500

    impresion de notas

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+
+int[] notas = { 15,18,12,17,20};
+
+//imprimir las notas
+
+for(int i=0; i<notas.Length; i++)
+{
+    Console.WriteLine(notas[i]);
+}
